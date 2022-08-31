3268cdf2f398aed1fcb1a4a15e710942b284f87c
Jeremy Reyes B <jjrb6@hotmail.com>
2022-08-31 10:04:01 -0500

    feat: Add user model

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,12 +6,15 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/jjrb3/go-app/middlewares"
 	"github.com/rs/cors"
 )
 
 // Handlers is a function that set port and listen the server.
 func Handlers() {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/register", middlewares.CheckDB(routers.Register)).Methods("POST")
 
 	PORT := os.Getenv("PORT")
 
---
+++ middlewares/check-db.go
@@ -0,0 +1,18 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/jjrb3/go-app/db"
+)
+
+func CheckDB(next http.HandlerFunc) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		if db.CheckConnection() == 0 {
+			http.Error(rw, "Connect lost with the Data base", 500)
+			return
+		}
+
+		next.ServeHTTP(rw, r)
+	}
+}
